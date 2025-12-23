750d9ae530c3903bee535dbacbf8bc4c357f83df
Miguel Martins <miguelmartins@Miguels-MacBook-Pro.local>
2025-12-24 00:35:53 +0100

    Add RGB classes

--- dataset_processing/load_datasets.py
+++ dataset_processing/load_datasets.py
@@ -32,6 +32,39 @@ class DietDataset(torch.utils.data.Dataset):
         return len(self.images)
 
 
+class DietRGBDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        images,
+        labels,
+        normalize=v2.Lambda(lambda x: x / 255.0),
+        augmentations=None,
+    ):
+        super().__init__()
+        self.images = images
+        self.augmentations = augmentations
+        if augmentations is None:
+            self.transform = v2.Compose(
+                [v2.ToImage(), v2.ToDtype(torch.float32, scale=False), normalize]
+            )
+        else:
+            self.transform = v2.Compose(
+                [
+                    v2.ToImage(),
+                    v2.ToDtype(torch.float32, scale=False),
+                    augmentations,
+                    normalize,
+                ]
+            )
+
+    def __getitem__(self, idx):
+        x = self.images[idx]
+        return self.transform(x), idx
+
+    def __len__(self):
+        return len(self.images)
+
+
 class DislibDataset(torch.utils.data.Dataset):
     def __init__(
         self,
@@ -50,7 +83,49 @@ class DislibDataset(torch.utils.data.Dataset):
             )
         else:
             self.transform = v2.Compose(
-                [v2.ToImage(), v2.ToDtype(torch.float32, scale=False), augmentations]
+                [
+                    v2.ToImage(),
+                    v2.ToDtype(torch.float32, scale=False),
+                    augmentations,
+                ]
+            )
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        x = self.images[idx]
+        y = self.labels[idx]
+
+        if self.transform is not None:
+            x = self.transform(x)
+        return x, y
+
+
+class RGBDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        images,
+        labels,
+        normalize=v2.Lambda(lambda x: x / 255.0),
+        augmentations=None,
+    ) -> None:
+        super().__init__()
+        self.images = images
+        self.labels = labels
+        self.augmentations = augmentations
+        if augmentations is None:
+            self.transform = v2.Compose(
+                [v2.ToImage(), v2.ToDtype(torch.float32, scale=False), normalize]
+            )
+        else:
+            self.transform = v2.Compose(
+                [
+                    v2.ToImage(),
+                    v2.ToDtype(torch.float32, scale=False),
+                    augmentations,
+                    normalize,
+                ]
             )
 
     def __len__(self):
